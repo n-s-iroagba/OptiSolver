a6b2a68201b69178aed1cd565221577bf9316b0d
n-s-iroagba <nnamdisolomon1@gmail.com>
2023-08-03 19:31:28 +0100

    fix pivot function bug

--- lpsolver-matlab/simplex_method/simplex.m
+++ lpsolver-matlab/simplex_method/simplex.m
@@ -1,6 +1,6 @@
 % input box dimension
+clear
 box_dimension= [5 135];
-
 objective_function_coefficients = get_objective_coefficient(box_dimension)
 
 number_of_constraint_equations = get_number_of_constraint_equations(box_dimension)
@@ -19,14 +19,15 @@ constant_matrix = get_constant_matrix(rows,box_dimension)
 most_negative_column = get_most_negative_crow_value(columns,c_row)
 most_negative_value = c_row(1,most_negative_column)
 
-while most_negative_value < 0
+
+while most_negative_valu > 0
         minimum_row_index = get_minimum_ratio(constant_matrix,constraint_equation_matrix,most_negative_column,rows)
         basic_variable_matrix = next_get_basic_variable_matrix(basic_variable_matrix,objective_function_coefficients,minimum_row_index,most_negative_column)
-
         constraint_equation_matrix = pivot(rows,constraint_equation_matrix,minimum_row_index,most_negative_column)
         c_row = get_crow(rows,columns,constraint_equation_matrix,basic_variable_matrix,objective_function_coefficients)
         most_negative_column = get_most_negative_crow_value(columns,c_row)
-        most_negative_value = crow(1,most_negative_column);
+        most_negative_value= c_row(1,most_negative_column);
+        
    end
 
 
