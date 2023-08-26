5fbf31fd3c10c547921de8cf65c9c18a4ffc1098
n-s-iroagba <nnamdisolomon1@gmail.com>
2023-08-26 08:42:50 +0100

    chore: initial test

--- lpSolver/App.js
+++ lpSolver/App.js
@@ -1,10 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, TextInput, View } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Hi there welcome to lpSolver.</Text>
+      <Text>Kindly enter the number of coefficients of the objective function you wish to optimise.</Text>
+      <TextInput keyboardType='numeric'/>
+      <Text>Enter the number of constraints</Text>
+      <TextInput keyboardType='numeric'/>
       <StatusBar style="auto" />
     </View>
   );
