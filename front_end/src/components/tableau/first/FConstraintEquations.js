c803b6bc4155a886b2532b1841f71e2394d224e0
n-s-iroagba <nnamdisolomon1@gmail.com>
2024-01-02 09:54:06 +0100

    [feat]:add first response tableau

---
+++ front_end/src/components/tableau/first/FConstraintEquations.js
@@ -0,0 +1,55 @@
+
+
+import { useState, useEffect } from 'react';
+import { MatrixContext } from '../../../features/solve_simplex/context/SimplexContext';
+import { useContext } from 'react';
+import '../tableau.css'
+import InitialObjectiveCoefficient from './InitialObjectiveCoefficient';
+import { createMultipleColumn } from '../../../utils/initialize/createBasicCoefficients';
+
+
+
+const dimensions = {
+  numberOfRows: 2,
+  numberOfColumns: 2
+}
+const ConstraintEquations = (props) => {
+  let { responseTableau, header } = useContext(MatrixContext)
+  
+  return (<>
+
+    <div className='center-tableau-wrapper'>
+      <InitialObjectiveCoefficient />
+      <table className='single-column'>
+        <thead >
+          <tr >{
+            header.map((variable, index) => {
+              return (<th key={index}>{index}</th>)
+            })}
+          </tr>
+        </thead>
+        <tbody className='tableau-body' >
+          {
+            responseTableau.constraintEquations.map((row, index) => {
+              return (<tr key={index}>{row}</tr>)
+            })
+          }
+        </tbody>
+
+      </table >
+      <tr className='c-row'>{
+        
+        responseTableau.cRrow.map((cell, index) => (
+            <th contentEditable key={index}>
+              {cell}
+            </th>))
+
+      }</tr>
+
+    </div>
+    </>
+    )
+}
+
+
+export default ConstraintEquations;
--- front_end/src/utils/initialize/createBasicCoefficients.js
+++ front_end/src/utils/initialize/createBasicCoefficients.js
@@ -2,54 +2,79 @@ import { preventNewLine } from "../preventNewLine";
 import { push } from "./push";
 
 
-const createSingleEntry = (tableau,setTableau,cells,variable,i,j)=>{
+const createSingleEntry = (tableau, setTableau, cells, variable, i, j) => {
     return cells.push(<td contentEditable onKeyDown={
-        (e) =>preventNewLine(e)} onInput={
-          (e) => {
-            push(e,i,variable,tableau,setTableau,j)}}>0</td>)
+        (e) => preventNewLine(e)} onInput={
+            (e) => {
+                push(e, i, variable, tableau, setTableau, j)
+            }}>0</td>)
 }
-export const createSingleColumn = async (numberOfRows,setRows,tableau,setTableau,variable)=>{
+export const createSingleColumn = (numberOfRows, setRows, tableau, setTableau, variable) => {
     let columnRows = []
     let tableauVariableValues = []
     for (let i = 0; i < numberOfRows; i++) {
-        createSingleEntry(tableau,setTableau,columnRows,variable,i)
-        tableauVariableValues.push(0)     
+        createSingleEntry(tableau, setTableau, columnRows, variable, i)
+        tableauVariableValues.push(0)
     }
-   let tempTab = tableau
-    tempTab[variable]=tableauVariableValues
-    await setTableau(tempTab)
-    await setRows(columnRows)
+    let tempTab = tableau
+    tempTab[variable] = tableauVariableValues
+    setTableau(tempTab)
+    setRows(columnRows)
     columnRows = []
-    tableauVariableValues=[] 
+    tableauVariableValues = []
 }
-export const createMultipleColumn = async (numberOfRows,numberOfColumns,setRows,tableau,setTableau,variable,setHeader)=>{
+export const createMultipleColumn = (numberOfRows, numberOfColumns, setRows, tableau, setTableau, variable, setHeader) => {
     let rows = []
-    let columns=[]
-    let headTag=[]
+    let columns = []
+    let headTag = []
     let tableauVariableValues = []
-    let tableauRows =[]
+    let tableauRows = []
     for (let i = 0; i < numberOfRows; i++) {
-    for (let j = 0; j < numberOfColumns; j++) {
-        createSingleEntry(tableau,setTableau,columns,variable,i)
-        tableauVariableValues.push(0)     
+        for (let j = 0; j < numberOfColumns; j++) {
+            createSingleEntry(tableau, setTableau, columns, variable, i)
+            tableauVariableValues.push(0)
+        }
+        rows.push(columns)
+        columns = []
+        tableauRows.push(tableauVariableValues)
+        tableauVariableValues = []
     }
-    rows.push(columns)
-    columns=[]
-    tableauRows.push(tableauVariableValues)
-    tableauVariableValues=[]
-}
-for (let j = 1; j <= numberOfColumns; j++) {
-    headTag.push(<th><td>X<sub>{j}</sub></td></th>)
+    for (let j = 1; j <= numberOfColumns; j++) {
+        headTag.push(<th><td>X<sub>{j}</sub></td></th>)
 
 
-}
-   let tempTab = tableau
-    tempTab[variable]=tableauRows
-    await setTableau(tempTab)
-    await setRows(rows)
+    }
+    let tempTab = tableau
+    tempTab[variable] = tableauRows
+    asetTableau(tempTab)
+    setRows(rows)
     columns = []
-    tableauRows=[]
+    tableauRows = []
     setHeader(headTag)
-    headTag=[]
+    headTag = []
 }
+export const createfirstTableauSingleColumn = (numberOfRows, setRows, inputTableau, variable) => {
+    const value = inputTableau[variable]
+    let rows = []
+    for (let i = 0; i < numberOfRows; i++) {
+        rows.push(<td>{value[i]}</td>)
+    }
+    setRows(rows)
+    rows = []
+}
+export const createFirstTableauMultiColumn = (numberOfRows, numberOfColumns, setRows, inputTableau, variable) => {
+    const value = inputTableau[variable]
+    let rows = []
+    let columns = []
+    for (let i = 0; i < numberOfRows; i++) {
+        for (let j = 0; j < numberOfColumns; j++) {
+            columns.push(<td>{value[j]}</td>)
+        }
+        rows.push(columns)
+        columns = []
+    }
+    setRows(rows)
+    rows = []
+}
+
 
