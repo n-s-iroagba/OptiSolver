29e9b28d85178987e803bc9e8992c433ab10c280
n-s-iroagba <nnamdisolomon1@gmail.com>
2024-01-02 18:44:00 +0100

    [feat]: write first iteration

---
+++ front_end/src/components/tableau/FirstIteration.js
@@ -0,0 +1,24 @@
+import React from "react";
+import FBasicCoefficients from "./first/FBasicCoefficients";
+import Ratio from "../tableau_components/input/Ratio";
+import FBasicVariables from "./first/FBasicVariables";
+import FConstraintEquations from "./first/FConstraintEquations";
+import FConstants from "./first/FConstants";
+
+
+
+
+
+const FirstIteration = () => {
+
+    return (
+        <div>
+            <FBasicCoefficients/>
+            <FBasicVariables/>
+            <FConstraintEquations/>
+            <FConstants/>
+            <Ratio/>
+        </div>
+    )
+}
+export default FirstIteration; 
