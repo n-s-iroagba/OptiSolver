c438a1fb0753c53bd570763eb4d1c160587b9fd6
n-s-iroagba <nnamdisolomon1@gmail.com>
2024-01-04 16:17:42 +0100

    [refactor]:refactored code

--- front_end/src/components/history_board/HistoryBoard.js
+++ front_end/src/components/history_board/HistoryBoard.js
@@ -1,9 +1,12 @@
 import React from "react";
-import HistoryBoardLinks from "../../components/dashboardnav/HistoryBoardLinks";
+import HistoryBoardLinks from "../dashboardnav/HistoryBoardLinks";
 
 const HistoryBoard = () => {
 return<>
 <div className="history-board">
+    <div className="history-board-header">
+        <h2>Problems solved</h2>
+    </div>
     <HistoryBoardLinks/>
     </div>
 </>
